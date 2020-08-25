5ede47ce9a7fb63d700e298f0673ec32051f428e
vanyadelova <happy.vanya.delova@gmail.com>
2020-08-25 19:09:40 +0300

    Formatting document

--- src/App.js
+++ src/App.js
@@ -46,78 +46,70 @@ class ComponentToPrint extends React.Component {
         ngt: newState
       });
     });
-
-  
-
   };
-
-
 
 
   render() {
     return (
-      
+
       <div>
-      <div>  
-      <a href="https://nextgatetech.com/" target="_blank" class="uk-icon" uk-icon="icon: world; ratio: 1.3"></a>
-      </div> 
-      <div className="uk-container pos">
-        <div className="navigation uk-visible@m" uk-sticky="bottom: #offset">
-          <a id="js-scroll-trigger" className="uk-button uk-button-text uk-align-left" href="#normal" uk-scroll>ngt</a>
-        
-
+        <div>
+          <a href="https://nextgatetech.com/" target="_blank" class="uk-icon" uk-icon="icon: world; ratio: 1.3"></a>
         </div>
-        <div className="nav uk-hidden@m" uk-sticky="bottom: #offset">
-          <a href="#normal" uk-tooltip="One" className="uk-icon uk-align-left" uk-icon="home"></a>
-          <a href="#adv" uk-tooltip="Two" className="uk-icon uk-align-right" uk-icon="plus"></a>
-        </div>
+        <div className="uk-container pos">
+          <div className="navigation uk-visible@m" uk-sticky="bottom: #offset">
+            <a id="js-scroll-trigger" className="uk-button uk-button-text uk-align-left" href="#normal" uk-scroll>ngt</a>
 
 
-        <section id="normal">
-          <div className="uk-container-small uk-center uk-align-center">
-         
+          </div>
+          <div className="nav uk-hidden@m" uk-sticky="bottom: #offset">
+            <a href="#normal" uk-tooltip="One" className="uk-icon uk-align-left" uk-icon="home"></a>
+            <a href="#adv" uk-tooltip="Two" className="uk-icon uk-align-right" uk-icon="plus"></a>
+          </div>
 
-            {this.state.ngt.map((fund_id) => {
-              return (
 
-    
+          <section id="normal">
+            <div className="uk-container-small uk-center uk-align-center">
 
-                <div className="cc">
-                  <div className="uk-child-width-1-1@m">
-                    <div className="uk-card uk-card-default uk-card-body card">
-                      <h3 className="uk-card-title fund_id">fund_id: {fund_id.fund_id}:</h3>
-                      <p className="fund_name">fund_name: {fund_id.fund_name}</p>
-                      <p className="date">date: {fund_id.date}</p>
-                      <p className="index">index: {fund_id.index}</p>
-                      <p className="nb_alerts">nb_alerts: {fund_id.nb_alerts}</p>
-                      <p className="report_status">report_status: {fund_id.report_status}</p>
-                      <p className="share_class_id">share_class_id: {fund_id.share_class_id}</p>
-                      <p className="share_class_name">share_class_name: {fund_id.share_class_name}</p>
-                      <p className="subfund_id">subfund_id: {fund_id.subfund_id}</p>
-                      <p className="subfund_name">subfund_name: {fund_id.subfund_name}</p>
-                      
 
+              {this.state.ngt.map((fund_id) => {
+                return (
+
+
+
+                  <div className="cc">
+                    <div className="uk-child-width-1-1@m">
+                      <div className="uk-card uk-card-default uk-card-body card">
+                        <h3 className="uk-card-title fund_id">fund_id: {fund_id.fund_id}:</h3>
+                        <p className="fund_name">fund_name: {fund_id.fund_name}</p>
+                        <p className="date">date: {fund_id.date}</p>
+                        <p className="index">index: {fund_id.index}</p>
+                        <p className="nb_alerts">nb_alerts: {fund_id.nb_alerts}</p>
+                        <p className="report_status">report_status: {fund_id.report_status}</p>
+                        <p className="share_class_id">share_class_id: {fund_id.share_class_id}</p>
+                        <p className="share_class_name">share_class_name: {fund_id.share_class_name}</p>
+                        <p className="subfund_id">subfund_id: {fund_id.subfund_id}</p>
+                        <p className="subfund_name">subfund_name: {fund_id.subfund_name}</p>
+
+
+                      </div>
                     </div>
                   </div>
-                </div>
 
 
 
-              )
-            })}
+                )
+              })}
 
-          </div>
-        </section>
-        
-        <section id="adv">
-          <div className="uk-container-small uk-center uk-align-center">
-        
+            </div>
+          </section>
 
-         
+          <section id="adv">
+            <div className="uk-container-small uk-center uk-align-center">
 
-          </div>
-        </section>
-      </div>
+            </div>
+          </section>
+        </div>
       </div>
 
     );
